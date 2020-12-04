0fad9648eb7ab8cc42165569edae703b3fe91e46
Jude <jantungkuno@gmail.com>
2020-12-03 21:01:29 -0500

    Completed Pseudo-Code for JavaScript

---
+++ Assets/script.js
@@ -0,0 +1,45 @@
+// Make a list of all the variables needed to respond to the user
+
+// Create an event listener to start the quiz
+startQuizButton.addEventListener("click",startQuiz);
+
+// Create start of quiz variables
+var startQuizButton = document.getElementById("startbtn");
+var startQuizDiv = document.getElementById("startpage");
+
+// Create middle of quiz vars (question vars)
+var questionsEl = document.getElementById("questions");
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
+
+// Create end of quiz vars
+
+// Create high score of quiz vars
+
+// Create questions and options for user to select from
+
+// Create needed variables for the timer, score, & questions correct
+
+// Create the function needed to go through the object array for the quiz questions and answers
+
+// Create the start quiz function that removes the start button
+
+// Create the timer
+
+// Create a function to show the number of correct answers after the last question
+
+// Create the submit button
+
+// Create a local storage with JSON to save the high score submissions
+ 
+// Create a display for the high scores to be stored in to show at the end of the quiz
+
+// Create a function to clear the high score list, and then create a new high score list from the local storage
+
+// Create a function to show the high score list without the other pages showing
+
+// Create a function to clear the high score storage 
+
+// Create a function to restart the quiz & timer
